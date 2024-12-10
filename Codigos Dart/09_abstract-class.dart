bac610a2f3df4c173374a13dec03427219d664c5
Julián Laterza <laterzajulian@gmail.com>
2024-12-10 00:04:12 -0300

    implements y extends

--- Codigos Dart/09_abstract-class.dart
+++ Codigos Dart/09_abstract-class.dart
@@ -1,8 +1,22 @@
 void main() {
   
-  //final windPlant = EnergyPlant();
-
+  final windPlant = WindPlant(initialEnergy: 100);
+  final nuclearPlant = NuclearPlant(energyLeft: 10000);
+  
+  print('Eolica: ${ chargePhone( windPlant ) }');
+  print('Nuclear: ${ chargePhone( nuclearPlant )}');
 }
+
+
+double chargePhone(EnergyPlant plant ) {
+  if (plant.energyLeft < 10){
+    throw Exception('Not enough energy');
+  }
+  
+  return plant.energyLeft - 10;
+}
+
+
 
 enum PlantType { nuclear, wind, water }
 
@@ -10,13 +24,40 @@ abstract class EnergyPlant {
   
 
   double energyLeft;
-  PlantType type;
+  final PlantType type;
   
   EnergyPlant({
     required this.energyLeft,
     required this.type
   });
   
-  void consumeEnergy( double amount);
+  void consumeEnergy( double amount); // no se termina de completar el cuerpo de la funcion porque supongamoste que cada planta calcula de una manera diferente.
+
+}
+
+
+class WindPlant extends EnergyPlant {
+  WindPlant({ required double initialEnergy})
+    : super(energyLeft: initialEnergy, type: PlantType.wind);
   
+  @override
+  void consumeEnergy( double amount){
+    energyLeft -= amount;
+  }
+}
+
+
+class NuclearPlant implements EnergyPlant {
+  
+  double energyLeft;
+  
+  @override
+  final PlantType type = PlantType.wind;
+  
+  NuclearPlant({ required this.energyLeft });
+  
+  @override
+  void consumeEnergy(double amount){
+    energyLeft -= (amount * 0.5);
+  }
 }
