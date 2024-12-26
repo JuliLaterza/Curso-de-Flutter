0be6e9620b653ed862d0a889b77a8211b7164eec
Julián Laterza <laterzajulian@gmail.com>
2024-12-26 12:41:15 -0300

    add sendmessage funct

--- Flutter/yes_no_app/lib/presentation/providers/chat_provider.dart
+++ Flutter/yes_no_app/lib/presentation/providers/chat_provider.dart
@@ -2,12 +2,19 @@ import 'package:flutter/material.dart';
 import 'package:yes_no_app/domain/entities/message.dart';
 
 class ChatProvider extends ChangeNotifier{ //notifica cuando hay cambios
-  List<Message> message = [
+  List<Message> messagesList = [
     Message(text: 'Hola!', fromWho: FromWho.me),
-    Message(text: 'Ya regresaste del trabajo?', fromWho: FromWho.me)
+    Message(text: 'Ya regresaste del trabajo?', fromWho: FromWho.me),
+    Message(text: 'Ya', fromWho: FromWho.me)
   ];
+
+
+  Future<void> sendMessage(String text) async {
+
+  final newMessage = Message(text: text, fromWho: FromWho.me);
+  messagesList.add(newMessage);
+  
+
+  }
 }
 
-Future<void> sendMessage(String text) async {
-  
-}
