78083adadf54773b80f8fd82d415b48b7b4afe03
Julián Laterza <laterzajulian@gmail.com>
2024-12-26 12:41:24 -0300

    add chatProvider

--- Flutter/yes_no_app/lib/presentation/screens/chat/chat_screen.dart
+++ Flutter/yes_no_app/lib/presentation/screens/chat/chat_screen.dart
@@ -1,7 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:yes_no_app/domain/entities/message.dart';
+import 'package:yes_no_app/main.dart';
+import 'package:yes_no_app/presentation/providers/chat_provider.dart';
 import 'package:yes_no_app/presentation/widgets/chat/her_message_dart.dart';
 import 'package:yes_no_app/presentation/widgets/chat/my_bubble_chart.dart';
 import 'package:yes_no_app/presentation/widgets/shared/message_field_box.dart';
+import 'package:provider/provider.dart';
+
 
 class ChatScreen extends StatelessWidget {
   const ChatScreen({super.key});
@@ -24,20 +29,33 @@ class ChatScreen extends StatelessWidget {
 
 
 class _ChatView extends StatelessWidget {
+
   
   @override
   Widget build(BuildContext context) {
+
+    final chatProvider = context.watch<ChatProvider>();
+
+
+
     return SafeArea(
       child: Column(
         children: [
-          Expanded(child: ListView.builder(itemCount: 100,itemBuilder: (context, index) {
-            return (index % 2 == 0)
-            ? const HerMessageBubble()
-            : const MyMessageBubble();
+          Expanded(child: ListView.builder(
+            itemCount: chatProvider.messagesList.length,
+            itemBuilder: (context, index) {
+              final message = chatProvider.messagesList[index];
+
+            return (message.fromWho == FromWho.hers)
+            ? HerMessageBubble( message: 'Hola')
+            : MyMessageBubble(message: message);
           },
           )),
           //const Text('Caja de texto')
-          const MessageFieldBox()
+          MessageFieldBox(
+            //onValue: (value) => chatProvider.sendMessage(value),
+            onValue: chatProvider.sendMessage,
+          )
         ],
       ),
     );
