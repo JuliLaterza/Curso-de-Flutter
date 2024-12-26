dc1a17adcb8ed54c4a110be481d597bcccdb6f38
Julián Laterza <laterzajulian@gmail.com>
2024-12-26 12:40:37 -0300

    update messageFieldBox
    Le creamos el metodo onvalue y valueChange

--- Flutter/yes_no_app/lib/presentation/widgets/shared/message_field_box.dart
+++ Flutter/yes_no_app/lib/presentation/widgets/shared/message_field_box.dart
@@ -1,10 +1,17 @@
 import 'package:flutter/material.dart';
 
 class MessageFieldBox extends StatelessWidget {
-  const MessageFieldBox({super.key});
+
+  final ValueChanged<String> onValue;
+
+  const MessageFieldBox({
+    super.key, 
+    required this.onValue
+    });
 
   @override
   Widget build(BuildContext context) {
+
     final textController = TextEditingController();
     final focusNode = FocusNode();
 
@@ -21,8 +28,8 @@ class MessageFieldBox extends StatelessWidget {
         icon: const Icon(Icons.send_outlined),
         onPressed: () {
           final textValue = textController.value.text;
-          print('button: $textValue');
           textController.clear();
+          onValue(textValue);
         },
       ),
     );
@@ -35,9 +42,9 @@ class MessageFieldBox extends StatelessWidget {
       controller: textController,
       decoration: inputDecoration,
       onFieldSubmitted: (value) {
-        print('Submit value $value');
         textController.clear();
         focusNode.requestFocus();
+        onValue(value);
       },
     );
   }
