e91fe6e770e056db98b66780db07713d29773ee7
Julián Laterza <laterzajulian@gmail.com>
2024-12-31 15:28:50 -0300

    update chat provider

--- Flutter/yes_no_app/lib/presentation/providers/chat_provider.dart
+++ Flutter/yes_no_app/lib/presentation/providers/chat_provider.dart
@@ -1,7 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:yes_no_app/config/helpers/get_yes_no_answer.dart';
 import 'package:yes_no_app/domain/entities/message.dart';
 
 class ChatProvider extends ChangeNotifier{ //notifica cuando hay cambios
+
+  final ScrollController chatScrollController = ScrollController();
+  final getYesNoAnswer = GetYesNoAnswer();
+
+
   List<Message> messagesList = [
     Message(text: 'Hola!', fromWho: FromWho.me),
     Message(text: 'Ya regresaste del trabajo?', fromWho: FromWho.me),
@@ -10,11 +16,32 @@ class ChatProvider extends ChangeNotifier{ //notifica cuando hay cambios
 
 
   Future<void> sendMessage(String text) async {
+    if(text.isEmpty) return;
 
-  final newMessage = Message(text: text, fromWho: FromWho.me);
-  messagesList.add(newMessage);
+    final newMessage = Message(text: text, fromWho: FromWho.me);
+    messagesList.add(newMessage);
+
+    if (text.endsWith('?')) {
+      herReply();
+    }
   
+    notifyListeners();
+    moveScrollToBottom();
+  }
 
+  Future<void> moveScrollToBottom() async {
+    await Future.delayed(const Duration(milliseconds: 50));
+    chatScrollController.animateTo(
+      chatScrollController.position.maxScrollExtent,
+      duration: const Duration(milliseconds: 300),
+      curve: Curves.easeOut
+    ); //SUPER IMPORTANTE PARA EL UX UI
+  }
+
+  Future<void> herReply() async {
+    final herMessage = await getYesNoAnswer.getAnswer();
+    messagesList.add(herMessage);
+    notifyListeners();
+    moveScrollToBottom() ;
   }
 }
-
