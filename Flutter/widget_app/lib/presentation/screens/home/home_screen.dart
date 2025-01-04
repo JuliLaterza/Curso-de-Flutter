5e1221402ea5376ea1fb308b38c0b1e799320e66
Julián Laterza <laterzajulian@gmail.com>
2025-01-04 20:16:18 -0300

    update home_screen

--- Flutter/widget_app/lib/presentation/screens/home/home_screen.dart
+++ Flutter/widget_app/lib/presentation/screens/home/home_screen.dart
@@ -1,47 +1,76 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 import 'package:widget_app/config/menu/menu_items.dart';
+// import 'package:widgets_app/presentation/screens/cards/cards_screen.dart';
+
 
 
 class HomeScreen extends StatelessWidget {
+
+  static const String name = 'home_screen';
+
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title:  const Text('Flutter + Material 3'),
+        title: const Text('Flutter + Material 3'),
       ),
       body: const _HomeView(),
     );
-
   }
 }
-
 
 class _HomeView extends StatelessWidget {
   const _HomeView();
 
   @override
   Widget build(BuildContext context) {
+
+
     return ListView.builder(
-      physics: const BouncingScrollPhysics(),
       itemCount: appMenuItems.length,
       itemBuilder: (context, index) {
         final menuItem = appMenuItems[index];
-      
-      final color = Theme.of(context).colorScheme;
 
-        return ListTile(
-          leading: Icon(menuItem.icon, color: color.primary),
-          trailing: Icon(Icons.arrow_forward_ios_rounded, color: color.primary),
-          title: Text(menuItem.title),
-          subtitle: Text(menuItem.subTitle),
-          onTap: () {
-            // navegar a otra pantalla
-            Navigator.pushNamed(context, menuItem.link);
-          },
-        );
-      }
-      );
+        return _CustomListTile(menuItem: menuItem);
+      },
+    );
+  }
+}
+
+class _CustomListTile extends StatelessWidget {
+  const _CustomListTile({
+    required this.menuItem,
+  });
+
+  final MenuItems menuItem;
+
+  @override
+  Widget build(BuildContext context) {
+
+    final colors = Theme.of(context).colorScheme;
+    
+    return ListTile(
+      leading: Icon( menuItem.icon, color: colors.primary ),
+      trailing: Icon( Icons.arrow_forward_ios_rounded, color:colors.primary ),
+      title: Text(menuItem.title),
+      subtitle: Text( menuItem.subTitle ),
+      onTap: () {
+        
+        // Navigator.of(context).push(
+        //   MaterialPageRoute(
+        //     builder: (context) => const ButtonsScreen(),
+        //   ),
+        // );
+        // Navigator.pushNamed(context, menuItem.link );
+
+        // context.pushNamed( CardsScreen.name );
+        context.push( menuItem.link );
+
+
+      },
+    );
   }
 }
