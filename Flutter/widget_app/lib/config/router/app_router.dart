2d4d626997d936334292ab340eb888366b764f2a
Julián Laterza <laterzajulian@gmail.com>
2025-01-04 20:14:42 -0300

    set go_router

--- Flutter/widget_app/lib/config/router/app_router.dart
+++ Flutter/widget_app/lib/config/router/app_router.dart
@@ -1,22 +1,27 @@
-
 import 'package:go_router/go_router.dart';
-import 'package:widget_app/presentation/screens/buttons/button_screen.dart';
-import 'package:widget_app/presentation/screens/home/home_screen.dart';
-import '../../presentation/screens/cards/cards_screen.dart';
+import 'package:widget_app/presentation/screens/screens.dart';
 
+// GoRouter configuration
 final appRouter = GoRouter(
+  initialLocation: '/',
   routes: [
+
     GoRoute(
       path: '/',
+      name: HomeScreen.name,
       builder: (context, state) => const HomeScreen(),
     ),
+
     GoRoute(
       path: '/buttons',
+      name: ButtonsScreen.name,
       builder: (context, state) => const ButtonsScreen(),
     ),
+
     GoRoute(
       path: '/cards',
+      name: CardsScreen.name,
       builder: (context, state) => const CardsScreen(),
     ),
-  ]
+  ],
 );
