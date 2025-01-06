5578d04f46350508ad35bc24064c86dec6dc2f03
Julián Laterza <laterzajulian@gmail.com>
2025-01-06 19:17:29 -0300

    update cards screen

--- Flutter/widget_app/lib/presentation/screens/cards/cards_screen.dart
+++ Flutter/widget_app/lib/presentation/screens/cards/cards_screen.dart
@@ -2,6 +2,16 @@ import 'package:flutter/material.dart';
 
 class CardsScreen extends StatelessWidget {
 
+  const cards = <Map<String, dynamic>>[
+    {'elevation': 2.0, 'color': Colors.red, 'label': 'One'},
+    {'elevation': 4.0, 'color': Colors.green, 'label': 'Two'},
+    {'elevation': 6.0, 'color': Colors.blue, 'label': 'Three'},
+    {'elevation': 8.0, 'color': Colors.yellow, 'label': 'Four'},
+    {'elevation': 10.0, 'color': Colors.orange, 'label': 'Five'},
+    {'elevation': 12.0, 'color': Colors.purple, 'label': 'Six'},
+  ];
+
+
   static const String name = 'cards_screen';
 
   const CardsScreen({super.key});
@@ -12,9 +22,21 @@ class CardsScreen extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Cards Screen'),
       ),
-      body: const Center(
-        child: Placeholder(),
-      ),
+      body: const _CardsView(),
+    );
+  }
+}
+
+
+class _CardsView extends StatelessWidget {
+  const _CardsView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        
+      ]
     );
   }
 }
